99e10d33d4ade93ba30b15b740f98aae73382374
Michael Oliveira <michaelsoliveira@gmail.com>
2025-03-06 21:45:19 -0300

    Atualização

--- src/controllers/TarefaController.ts
+++ src/controllers/TarefaController.ts
@@ -1,11 +1,46 @@
 import { Request, Response } from "express";
 import tarefaService from "../services/TarefaService";
+import { prismaClient } from "../database/prismaClient";
 
 export class TarefaController {
+    async store(request: Request, response: Response) : Promise<Response> {
+        try {
+            const tarefa = await tarefaService.create(request.body);
+            return response.json({
+                error: false,
+                data: tarefa,
+                message: `Tarefa ${tarefa.descricao} foi cadastrada com SUCESSO!!!`
+            })
+        } catch (error: any) {
+            console.log(error.message);
+            return response.json({
+                error: true,
+                message: error.message
+            });
+        }
+    }
+
+    async update(request: Request, response: Response) : Promise<any> {
+        const { id } = request.params
+        try {
+            const tarefa = await tarefaService.update(id, request.body);
+            return response.json({
+                error: false,
+                data: tarefa,
+                message: `Tarefa ${tarefa.descricao} foi atualizada com SUCESSO!!!`
+            })
+        } catch (error: any) {
+            console.log(error.message);
+            return response.json({
+                error: true,
+                message: error.message
+            });
+        }
+    }
     async findAll(request: Request, response: Response) : Promise<any> {
         try {
             const tarefas = await tarefaService.getAll();
-
+            
             return response.json({
                 error: false,
                 data: tarefas
@@ -16,5 +51,30 @@ export class TarefaController {
                 message: error.message
             });
         }
+    }
+
+    async delete(request: Request, response: Response) : Promise<any> {
+        const { id } = request.params;
+        const res: any = await tarefaService.delete(id);
+        return response.status(200).json({
+            error: res.error,
+            message: res.message
+        })
+    }
+
+    async findOne(request: Request, response: Response) : Promise<any> {
+        const { id } = request.params as any;
+        try {
+            const tarefa = await tarefaService.findById(id);
+            return response.status(200).json({
+                tarefa
+            })
+        } catch(e: any) {
+            return {
+                error: true,
+                message: e.message
+            }
+        }
+        
     }
 }
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,13 @@
 import { Router } from "express"
 import { TarefaController } from "../controllers/TarefaController";
 
-const routes = Router();
+const express = require('express')
+const routes = express.Router();
 
-routes.get('/tarefa', new TarefaController().findAll);
+routes.get('/tarefa/', new TarefaController().findAll);
+routes.post('/tarefa', new TarefaController().store);
+routes.get('/tarefa/:id', new TarefaController().findOne);
+routes.put('/tarefa/:id', new TarefaController().update);
+routes.delete('/tarefa/:id', new TarefaController().delete);
 
 export default routes;
