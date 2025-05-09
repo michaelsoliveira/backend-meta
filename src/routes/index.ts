55f151bde7129a35a646545d20a918d85b031601
Michael Oliveira <michaelsoliveira@gmail.com>
2025-05-08 21:24:16 -0300

    Atualizações

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,7 +1,7 @@
-import { Router } from "express"
 import { UserController } from "../controllers/UserController";
 import { AuthController } from "../controllers/AuthController";
 import { Authentication } from "../middleware/auth.middleware";
+import ClienteController from "../controllers/ClienteController";
 
 const express = require('express')
 const routes = express.Router();
@@ -11,6 +11,11 @@ routes.post('/users', new UserController().store);
 routes.get('/users/:id', new UserController().findOne);
 routes.put('/users/:id', new UserController().update);
 routes.delete('/users/:id', new UserController().delete);
+routes.get("/cliente", ClienteController.findAll);
+routes.get("/cliente/:id", ClienteController.findById);
+routes.post("/cliente", ClienteController.create);
+routes.put("/cliente/:id", ClienteController.update);
+routes.delete("/cliente/:id", ClienteController.delete);
 
 routes.post('/auth/login', new AuthController().login)
 
